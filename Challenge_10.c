3a51310114093cbf3ca340aa30ddb0f6c688f505
Elbouhmydydabdelmoughit <elbouhmydydabdelmoughit@gmail.com>
2021-11-12 15:57:48 +0100

    Changed challange_10

--- Challenge_10.c
+++ Challenge_10.c
@@ -2,5 +2,13 @@
 #include <math.h>
 int main()
 {
+    float r,cir,pi;
+    pi=3.14;
+
+    printf ("Entrez le r \n ");
+    scanf("%f",&r);
+    cir = 2*pi*r;
+
+    printf("le cir est : %f",cir);
     return 0;
 }
