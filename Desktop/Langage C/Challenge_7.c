73c02847c1267bb981f4073f8b655197d6aa8d49
Elbouhmydydabdelmoughit <elbouhmydydabdelmoughit@gmail.com>
2021-11-12 14:43:10 +0100

    Change

--- Desktop/Langage C/Challenge_7.c
+++
@@ -1,8 +0,0 @@
-int main()
-{
-   int a,b;
-   printf("Entrez les 2 numero \n");
-   scanf("%d%d",&a,&b);
-   printf("le resultat est :\n a+b=%d\na-b=%d\na*b=%d\na/b=%d\na%b=%d",a+b,a-b,a*b,a/b,a%b);
-   return 0; 
-}
