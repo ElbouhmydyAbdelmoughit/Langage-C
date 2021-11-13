64ff88c0a7908e16e4fbb17bf906df16c94ec377
Elouhmydy <elbouhmydyabdelmoughit@gmail.com>
2021-11-13 11:27:07 +0000

    Challenge_12

---
+++ Challenge_12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int nbr,temp;
+
+    printf("Entrez votre nombre \n");
+    scanf("%d",&nbr);
+    nbr[0] = temp;
+    temp = nbr[2];
+    nbr[0] = temp;
+
+    printf("le ruseltat est :%d",nbr);
+
+    return 0;
+}
